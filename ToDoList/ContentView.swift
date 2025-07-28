11542055a0625afcb7b369682dbc344eebf416e1
Kate <kmc.macguire@gmail.com>
2025-07-28 11:32:03 -0600

    Iteration 0 complete

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    
     var body: some View {
         VStack {
             HStack{
@@ -15,8 +17,10 @@ struct ContentView: View {
                     .font(.system(size:40))
                     .fontWeight(.black)
                 Spacer()
-                
                 Button {
+                    withAnimation {
+                        showNewTask = true
+                    }
                 } label: {
                     Text("+")
                         .font(.title)
@@ -26,6 +30,9 @@ struct ContentView: View {
         }
         .padding()
         Spacer()
+        if showNewTask {
+            NewToDo()
+        }
     }
 }
 
